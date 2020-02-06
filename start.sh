430bec948e8245e82dc43bb82960b93a0e8848fb
Drew Brantley <brantley@us.ibm.com>
2020-02-06 15:45:28 -0600

    change from absolute dir to current dir

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 clear 
-docker run --name couchdb -p 4369:4369 -p 5984:5984 -p 9100:9100 -v /Users/Drew/playin/docker_couchdb/data:/opt/couchdb/data -d couchdb:latest
+docker run --name couchdb -p 4369:4369 -p 5984:5984 -p 9100:9100 -v $(pwd):/opt/couchdb/data -d couchdb:latest
 
